cec6748b7ab5c892cd309c7c20d95572b2da1846
Mitchell Stowman <92927307+StowCode@users.noreply.github.com>
2022-02-21 18:35:59 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -48,6 +48,19 @@ observer.observe(portfolio);
 observer.observe(weather);
 observer.observe(todo);
 
+/* parallax mouse move */
+
+document.addEventListener("mousemove", parallax);
+function parallax(e){
+    this.querySelectorAll('.layer').forEach(layer => {
+        const speed = layer.getAttribute('data-speed')
+
+        const x = (window.innerWidth - e.pageX*speed)/100
+        const y = (window.innerHeight - e.pageY*speed)/100
+
+        layer.style.transform = `translateX(${x}px) translateY(${y}px)`
+    })
+}
 
 
 /* parallax effects 
